b4fe03d86b10a29da060ca6bbcc6cc390874eadd
joepwijnhoven <joep_wijnhoven@hotmail.com>
2018-03-21 14:28:10 +0100

    onze simpleestimator erin gezet

--- include/SimpleEstimator.h
+++ include/SimpleEstimator.h
@@ -11,12 +11,26 @@
 class SimpleEstimator : public Estimator {
 
     std::shared_ptr<SimpleGraph> graph;
+    uint32_t sampleSize;
+    std::vector<uint32_t> nr_edges_in;
+    std::vector<uint32_t> nr_edges_out;
+    std::vector<uint32_t> nr_label_occurences;
+    std::vector<uint32_t> array;
+    std::vector<std::vector<std::tuple<int, int>>> tabels;
+    std::vector<std::vector<uint32_t>>  uniqueIN;
+    std::vector<std::vector<uint32_t>>  uniqueOUT;
+    std::vector<std::vector<uint32_t>>  AttributeCountIN;
+    std::vector<std::vector<uint32_t>>  AttributeCountOUT;
+    std::vector<uint32_t> thresholdsIN;
+    std::vector<uint32_t> thresholdsOUT;
+    std::vector<int> joinAttributes;
 
 public:
     explicit SimpleEstimator(std::shared_ptr<SimpleGraph> &g);
     ~SimpleEstimator() = default;
 
     void prepare() override ;
+    std::vector<int> estimatePath(RPQTree *q);
     cardStat estimate(RPQTree *q) override ;
 
 };
--- src/SimpleEstimator.cpp
+++ src/SimpleEstimator.cpp
@@ -9,17 +9,73 @@ SimpleEstimator::SimpleEstimator(std::shared_ptr<SimpleGraph> &g){
 
     // works only with SimpleGraph
     graph = g;
+    nr_label_occurences.resize(graph.get()->getNoLabels());
+    array.resize(graph.get()->getNoLabels());
+    tabels.resize(graph.get()->getNoLabels());
+    uniqueIN.resize(graph.get()->getNoLabels());
+    uniqueOUT.resize(graph.get()->getNoLabels());
+    AttributeCountIN.resize(graph.get()->getNoLabels());
+    AttributeCountOUT.resize(graph.get()->getNoLabels());
+    thresholdsIN.resize(graph.get()->getNoLabels());
+    thresholdsOUT.resize(graph.get()->getNoLabels());
+    sampleSize = ((graph.get()->getNoEdges() * 2) / graph->getNoLabels()) / 5;
 }
 
 void SimpleEstimator::prepare() {
 
     // do your prep here
+    for(int k =0; k< array.size(); k++){
+        array[k] = 0;
+    }
 
+    for(uint32_t source = 0; source < graph->getNoVertices(); source++) {
+        for (auto labelTarget : graph->adj[source]) {
+            auto label = labelTarget.first;
+            auto target = labelTarget.second;
+            tabels[label].emplace_back(std::make_tuple(source, target));
+            array[label] +=1;
+            //AttributeCountIN[label][source] +=1;
+            //AttributeCountOUT[label][target] +=1;
+            if(!(std::find(std::begin(uniqueIN[label]), std::end(uniqueIN[label]), source) != std::end(uniqueIN[label]))) {
+                uniqueIN[label].push_back(source);
+            }
+            if(!(std::find(std::begin(uniqueOUT[label]), std::end(uniqueOUT[label]), target) != std::end(uniqueOUT[label]))) {
+                uniqueOUT[label].push_back(target);
+            }
+        }
+    }
+}
+std::vector<int> SimpleEstimator::estimatePath(RPQTree *q) {
+    if(q->isLeaf()){
+        std::smatch matches;
+        std::regex directLabel (R"((\d+)\+)");
+        std::regex inverseLabel (R"((\d+)\-)");
+        if(std::regex_search(q->data, matches, directLabel)) {
+            auto label = (uint32_t) std::stoul(matches[1]);
+            std::vector<int> test {static_cast<int>(uniqueIN[label].size()), static_cast<int>(uniqueOUT[label].size()), array[label], label, 1};
+            return test;
+        } else if(std::regex_search(q->data, matches, inverseLabel)) {
+            auto label = (uint32_t) std::stoul(matches[1]);
+            std::vector<int> test {static_cast<int>(uniqueOUT[label].size()), static_cast<int>(uniqueIN[label].size()), array[label], label, 0};
+            return test;
+        } else {
+            std::cerr << "Label parsing failed!" << std::endl;
+            std::vector<int> test;
+            return test;
+        }
+    }
+    if(q->isConcat()) {
+        auto left = estimatePath(q->left);
+        auto right = estimatePath(q->right);
+        std::vector<int> test {left[0], right[1], (int) std::min((left[2] * (((double)right[2])/((double)right[0]))), right[2] * ((double)left[2]/(double)left[1]))};
+        return test;
+    }
+    return {};
 }
 
 cardStat SimpleEstimator::estimate(RPQTree *q) {
-
-    // perform your estimation here
-
-    return cardStat {0, 0, 0};
+    std::vector<int> testvar = estimatePath(q);
+    testvar[0] = std::min(testvar[0], testvar[2]);
+    testvar[1] = std::min(testvar[1], testvar[2]);
+    return cardStat {testvar[0],testvar[2],testvar[1]};
 }
